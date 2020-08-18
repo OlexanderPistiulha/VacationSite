967af5a00d346bc5369a9086651f7c4405a1e2cf
AlexanderPistiulha <olexanderfreelance@gmail.com>
2020-08-18 14:08:09 +0300

    menu-mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', function () {
         document.querySelectorAll(".burger__line")[0].classList.toggle("burger__one-active");
         document.querySelectorAll(".burger__line")[1].classList.toggle("burger__two-active");
         document.querySelectorAll(".burger__line")[2].classList.toggle("burger__three-active");
-        //document.querySelector(".mobile-nav").classList.toggle("mobile-nav--active");
+        document.querySelector(".menu-mobile").classList.toggle("menu-mobile__active");
     });
 
 });
