2acf1aa4efc14874ec1a235353183f074cd0fb40
AlexanderPistiulha <olexanderfreelance@gmail.com>
2020-08-18 12:53:51 +0300

    menu desktop + burger

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+
+    let burder = document.querySelector('.burger');
+
+    burder.addEventListener('click', function () {
+        document.querySelectorAll(".burger__line")[0].classList.toggle("burger__one-active");
+        document.querySelectorAll(".burger__line")[1].classList.toggle("burger__two-active");
+        document.querySelectorAll(".burger__line")[2].classList.toggle("burger__three-active");
+        //document.querySelector(".mobile-nav").classList.toggle("mobile-nav--active");
+    });
+
+});
